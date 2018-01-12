649164b521d13d0cd5b7707ea4f4c7253921498d
Caryn McCarthy <cmccarthy15@gmail.com>
2018-01-12 13:00:06 -0500

    playing around with photos of platanos?

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View, Image } from 'react-native';
 
-export default class App extends React.Component {
+export default class HelloWorldApp extends Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Hello world!</Text>
+        <Image source={pic} style={{ width: 193, height: 110 }} />
+        <Text>Well this is pretty sexy</Text>
       </View>
     );
   }
@@ -19,5 +22,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
